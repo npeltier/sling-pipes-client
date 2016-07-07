5d35036ce565e62d4531523731bb8be3a5992589
jsbst <karyu@Kamikaze-29.(none)>
2016-07-07 11:48:55 +0200

    (fixes #22) allowing scrolling in the pipecontainer

--- front/src/main/javascript/pipes.js
+++ front/src/main/javascript/pipes.js
@@ -2,6 +2,8 @@ Pipes = {
     handleResults : function(data) {
         var list = $(".results ul");
         $(".results span.total").text(data.size);
+        if(data.size>3){
+        }
         $.each(data.items, function(idx, item){
             $("<li/>").addClass("list-group-item").text(item).appendTo(list);
         });
@@ -12,7 +14,6 @@ Pipes = {
     },
     emptyResultsList : function(data) {
             $( ".list-group-item" ).remove();
-
     }
 };
 
@@ -20,7 +21,10 @@ $(document).ready(function(){
     $.getJSON(Pipes.retrievePipePath() + ".json").then(Pipes.handleResults);
 
     $('.execute').click(function(){
-    $.getJSON(Pipes.retrievePipePath() + ".json").then(Pipes.emptyResultsList);
+    Pipes.emptyResultsList();
+    document.styleSheets[0].addRule('.subpipe:before')({
+         "border-left-color":"#b2d526"
+    });
       $.ajax({
          url: Pipes.retrievePipePath() + ".json",
          type:'post',
@@ -37,12 +41,12 @@ $(document).ready(function(){
             data = {};
         data[property] = value;
         $.ajax({
-                 url: path,
-                 type:'post',
-                 data: data,
-                 success: function () {
-                             console.log("ok");
-                   }
+             url: path,
+             type:'post',
+             data: data,
+             success: function () {
+                         console.log("ok");
+               }
          });
     });
 });
