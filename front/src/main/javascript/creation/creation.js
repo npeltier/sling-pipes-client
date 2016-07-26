ff7efe00a2b13869204be0b7fa1055d2fa0227b6
Jean-Sebastien Sainte Rose <jean-sebastien.sainte-rose@laposte.net>
2016-07-26 16:12:28 +0200

    beginning of issue #40 (first jasmin test with a split function in creation.js)

--- front/src/main/javascript/creation/creation.js
+++ front/src/main/javascript/creation/creation.js
@@ -1,5 +1,12 @@
 Pipes.Creation = {
     "buildJson" : function() {
-        return {};
+    },
+    /**
+     * parse a piped command and return a list of subpipes
+     **/
+    "parseCommand" : function (command) {
+        var subCommands = command.trim().split(/[\r\s]*\|[\r\s]*/gi);
+        if (subCommands.indexOf("")!=-1){ return null;}
+        return subCommands;
     }
 };
--- front/src/test/javascript/specs/CreationSpec.js
+++ front/src/test/javascript/specs/CreationSpec.js
@@ -1,6 +1,11 @@
 describe("Creation test suite", function() {
-  it("buildJson works correctly", function() {
-    expect(Pipes.Creation.buildJson()).toEqual({});
+  it("parseCommand should parse a command and return a list of subpipes", function() {
+    expect(Pipes.Creation.parseCommand("find blah|rm blah")).toEqual(["find blah","rm blah"]);
+    expect(Pipes.Creation.parseCommand(" find blah | rm blah ")).toEqual(["find blah","rm blah"]);
+    expect(Pipes.Creation.parseCommand("find blah")).toEqual(["find blah"]);
+    expect(Pipes.Creation.parseCommand("")).toBe(null);
+    expect(Pipes.Creation.parseCommand("| blah")).toBe(null);
+    expect(Pipes.Creation.parseCommand("blah |")).toBe(null);
   });
 });
 
