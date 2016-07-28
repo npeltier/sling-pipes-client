30d4934b5dc99e707fe1231f50965a593349e668
Jean-Sebastien Sainte Rose <jean-sebastien.sainte-rose@laposte.net>
2016-07-28 14:32:43 +0200

    (fixes #40) first pipe creation.

--- bundle/src/main/java/org/apache/sling/pipes/client/MappingUse.java
+++ bundle/src/main/java/org/apache/sling/pipes/client/MappingUse.java
@@ -3,6 +3,10 @@ package org.apache.sling.pipes.client;
 import org.apache.commons.collections.IteratorUtils;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ValueMap;
+import org.apache.sling.commons.json.JSONException;
+import org.apache.sling.commons.json.JSONObject;
+import org.apache.sling.commons.json.io.JSONRenderer;
 import org.apache.sling.scripting.sightly.pojo.Use;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +14,7 @@ import org.slf4j.LoggerFactory;
 import javax.script.Bindings;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class MappingUse implements Use {
     private static Logger logger = LoggerFactory.getLogger(MappingUse.class);
@@ -29,5 +34,18 @@ public class MappingUse implements Use {
 
     public List<Resource> getChildren() {
         return children;
+    }
+
+    public String getJsonDump() throws JSONException {
+        JSONObject dump = new JSONObject();
+        for (Resource pipe : getChildren()){
+            ValueMap properties = pipe.adaptTo(ValueMap.class);
+            JSONObject pipeObject = new JSONObject();
+            for (Map.Entry<String, Object> entry : properties.entrySet()){
+                pipeObject.put(entry.getKey(), entry.getValue());
+            }
+            dump.put(pipe.getName(), pipeObject);
+        }
+        return dump.toString();
     }
 }
--- front/src/test/javascript/specs/CreationSpec.js
+++ front/src/test/javascript/specs/CreationSpec.js
@@ -1,4 +1,27 @@
 describe("Creation test suite", function() {
+var mapping = {
+      "$":{
+        "jcr:primaryType":"nt:unstructured",
+        "sling:resourceType":"sling/pipes-client/mapping",
+        "pipeType":"slingPipes/slingQuery",
+        "name":"$",
+        "args":["path","expr"]
+      },
+      "rm":{
+        "jcr:primaryType":"nt:unstructured",
+        "sling:resourceType":"sling/pipes-client/mapping",
+        "pipeType":"slingPipes/rm",
+        "name":"rm",
+        "args":["path"]
+      },
+      "base":{
+      "jcr:primaryType":"nt:unstructured",
+      "sling:resourceType":"sling/pipes-client/mapping",
+      "pipeType":"slingPipes/base",
+      "name":"base"
+      }
+    };
+
   it("parseCommand should parse a command and return a list of subpipes", function() {
     expect(Pipes.Creation.parseCommand("find blah|rm blah")).toEqual(["find blah","rm blah"]);
     expect(Pipes.Creation.parseCommand(" find blah | rm blah ")).toEqual(["find blah","rm blah"]);
@@ -7,5 +30,58 @@ describe("Creation test suite", function() {
     expect(Pipes.Creation.parseCommand("| blah")).toBe(null);
     expect(Pipes.Creation.parseCommand("blah |")).toBe(null);
   });
-});
 
+  it("parseSubCommand should parse a subcommand and return a JSONObject", function() {
+
+    expect(Pipes.Creation.parseSubCommand("rm   /content/foo", mapping)).toEqual({
+        "jcr:primaryType":"nt:unstructured",
+        "sling:resourceType":"slingPipes/rm",
+        "path":"/content/foo"
+    });
+    expect(Pipes.Creation.parseSubCommand("rm", mapping)).toEqual({
+        "jcr:primaryType":"nt:unstructured",
+        "sling:resourceType":"slingPipes/rm"
+    });
+    expect(Pipes.Creation.parseSubCommand("$  /content/foo  cq:Page", mapping)).toEqual({
+        "jcr:primaryType":"nt:unstructured",
+        "sling:resourceType":"slingPipes/slingQuery",
+        "path":"/content/foo",
+        "expr":"cq:Page"
+    });
+    expect(Pipes.Creation.parseSubCommand("base",mapping)).toEqual({
+        "jcr:primaryType":"nt:unstructured",
+         "sling:resourceType":"slingPipes/base"
+    });
+  });
+
+  it("buildJson should convert a command into corresponding container pipe configuration",function(){
+    expect(Pipes.Creation.buildJson("$  /content/foo  cq:Page | rm",mapping)).toEqual({
+        "jcr:primaryType":"nt:unstructured",
+        "sling:resourceType":"slingPipes/container",
+        "conf":{"jcr:primaryType":"sling:OrderedFolder",
+            "$":{
+                 "jcr:primaryType":"nt:unstructured",
+                 "sling:resourceType":"slingPipes/slingQuery",
+                 "path":"/content/foo",
+                 "expr":"cq:Page"
+            },
+            "rm":{
+                 "jcr:primaryType":"nt:unstructured",
+                 "sling:resourceType":"slingPipes/rm"
+            }
+        }
+    });
+    expect(Pipes.Creation.buildJson("$  /content/foo" ,mapping)).toEqual({
+            "jcr:primaryType":"nt:unstructured",
+            "sling:resourceType":"slingPipes/container",
+            "conf":{"jcr:primaryType":"sling:OrderedFolder",
+                "$":{
+                     "jcr:primaryType":"nt:unstructured",
+                     "sling:resourceType":"slingPipes/slingQuery",
+                     "path":"/content/foo"
+                }
+            }
+        });
+  });
+
+});
